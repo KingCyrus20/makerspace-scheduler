197c504c8e3eeb20290c842aa28dc1803c730920
KingCyrus20 <keyon@keyoninfo.com>
2017-11-05 23:57:49 -0600

    Added Employee class, functionality to add and remove employees.

--- MakerspaceSchedulerProject/MakerspaceSchedulerProject/MainWindow.xaml.cs
+++ MakerspaceSchedulerProject/MakerspaceSchedulerProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,32 @@ namespace MakerspaceSchedulerProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static ObservableCollection<Employee> employeeList;
         public MainWindow()
         {
             InitializeComponent();
+            employeeList = new ObservableCollection<Employee>();
+            EmployeeSelector.ItemsSource = employeeList;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            employeeList.Add(new Employee(EmployeeSelector.Text));
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (employeeList.Count() > 0)
+            {
+                foreach (Employee emp in employeeList)
+                {
+                    if (emp.Name.Equals(EmployeeSelector.Text))
+                    {
+                        employeeList.Remove(emp);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
